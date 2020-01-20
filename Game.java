d2f6c52eb5aeda4fa33046dedcf7adb1a44d3eee
Tobeboy10 <tobeboy10@gmail.com>
2020-01-19 17:53:22 -0800

    Added save system and more player variables.

--- Game.java
+++ Game.java
@@ -5,10 +5,10 @@ public class Game {
     Scanner reader = new Scanner(System.in);
     Player player;
 
-    public Game() {
+    public Game() { //Essentially the main menu
         System.out.println("Welcome to the world's first VRMMORPG...");
         System.out.println("");
-        System.out.println("        Earth Online"); //come up with a better fake VRMMORPG name
+        System.out.println("        Ulibis Online");
         System.out.println("");
         File saveGame = new File("SaveGame.txt");
         Boolean exists = saveGame.exists();
@@ -17,26 +17,79 @@ public class Game {
             if (reader.nextLine().equals("y")) {
                 loadSave();
             } else {
-                startGame();
+                newGame();
             }
         } else {
-            System.out.println("Would you like to begin? y/n");
-        }
-        if (reader.nextLine().equals("y")) {
-            startGame();
-        } else {
-            System.out.println("Come again!");
+            newGame();
         } 
     }
 
-    private void startGame() {
+    private void newGame() { //Begins a new game, starting with character creation.
+        System.out.println("");
+        System.out.println("// Character Creation \\\\");
         System.out.println("What is your name?");
         String name = reader.nextLine();
-        player = new Player(name);
+        player = new Player(name, 10, 0, 10, 1, 0); 
+        System.out.println("");
+        System.out.println("Welcome " + player.getName() + ".");
+        System.out.println("You are level " + player.getLevel() + " with " + player.getGold() + " gold and " + player.getRations() + " rations.");
+        System.out.println("You are well.");
+        System.out.println("");
+        saveGame();
+    }
+    
+    private void saveGame() { //Overwrites save file "SaveGame.txt" using player data.
+        try {
+            FileWriter writer = new FileWriter("SaveGame.txt", false);
+            writer.write(player.getName());
+            writer.write("\r\n");
+            writer.write(Integer.toString(player.getLevel()));
+            writer.write("\r\n");
+            writer.write(Integer.toString(player.getExperience()));
+            writer.write("\r\n");
+            writer.write(Integer.toString(player.getGold()));
+            writer.write("\r\n");
+            writer.write(Integer.toString(player.getRations()));
+            writer.write("\r\n");
+            writer.write(Integer.toString(player.getHealth()));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void loadSave() { //Loads save file "SaveGame.txt" and sets player data based on the save file.
+        try {
+            FileReader reader = new FileReader("SaveGame.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            player = new Player();
+            String line;
+            if ((line = bufferedReader.readLine()) != null) {
+                player.setName(line);
+            } if ((line = bufferedReader.readLine()) != null) {
+                player.setLevel(Integer.parseInt(line));
+            } if ((line = bufferedReader.readLine()) != null) {
+                player.setExperience(Integer.parseInt(line));
+            } if ((line = bufferedReader.readLine()) != null) {
+                player.setGold(Integer.parseInt(line));
+            } if ((line = bufferedReader.readLine()) != null) {
+                player.setRations(Integer.parseInt(line));
+            } if ((line = bufferedReader.readLine()) != null) {
+                player.setHealth(Integer.parseInt(line));
+            }
+            reader.close();
+
+            System.out.println("Welcome " + player.getName() + ".");
+            System.out.println("You are level " + player.getLevel() + " with " + player.getGold() + " gold and " + player.getRations() + " rations.");
+            System.out.println("You are " + player.checkWounds());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadSave() {
-        //will eventually load a save file
-    }
 
+    //TODO Game progression
+
+    
 }
--- Player.java
+++ Player.java
@@ -1,13 +1,127 @@
-
 public class Player {
-    public String name;
-    public int gold;
-    public int rations;
+    private String name;
+    private int gold;
+    private int rations;
+    private int health;
+    private int maxHealth;
+    private int level;
+    private int experience;
 
+    //Constructors
     public Player(){
 
     }
-    public Player(String n){
+    public Player(String n, int g, int r, int h, int l, int e){
         name = n;
+        gold = g;
+        rations = r;
+        health = h;
+        level = l;
+        maxHealth = 8 + (level*2);
+        experience = e;
+    }
+
+    //Name commands
+    public String getName() {
+        return name;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+    
+    //Gold commands
+    public int getGold(){
+        return gold;
+    }
+    public void setGold(int g) {
+        gold = g;
+    }
+    public void changeGold(int g) {
+        gold += g;
+    }
+    
+    //Ration commands
+    public int getRations() {
+        return rations;
+    }
+    public void setRations(int r) {
+        rations = r;
+    }
+    public void changeRations(int r) {
+        rations += r;
+    }
+    
+    //Health commands
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int h) {
+        health = h;
+    }
+    public void changeHealth(int h) {
+        health += h;
+        if (health > maxHealth) {
+            health = maxHealth;
+        }
+    }
+    
+    //MaxHealth commands
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    public void setMaxHealth() {
+        maxHealth = 8 + (level*2);
+    }
+    
+    //Level commands
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int l) {
+        level = l;
+    }
+    
+    //Experience commands
+    public int getExperience() { 
+        return experience;
+    }
+    public void setExperience(int e) {
+        experience = e;
+        checkLevelUp();
+    }
+    public void changeExperience(int e) {
+        experience += e;
+        checkLevelUp();
+    }
+    
+    //LevelUp command
+    public void checkLevelUp() { 
+        if (experience >= 15 + (level*5)) {
+            level += 1;
+            experience -= (15+(level*5));
+            setMaxHealth();
+            setHealth(maxHealth);
+            System.out.println("");
+            System.out.println("!!! Your level has increased. You are now level " + level + ". !!!");
+            System.out.println("You feel well.");
+            System.out.println("");
+        }
+    }
+    
+    //CheckWounds command
+    public String checkWounds() { 
+        if (health >= maxHealth * 0.9) {
+            return "well.";
+        } else if (health >= maxHealth * 0.75) {
+            return "slightly wounded.";
+        } else if (health >= maxHealth * 0.5) {
+            return "moderately wounded.";
+        } else if (health >= maxHealth * 0.25) {
+            return "heavily wounded.";
+        } else if (health >= 1) {
+            return "mortally wounded.";
+        } else {
+            return "dead."; //TODO Death
+        }
     }
 }
