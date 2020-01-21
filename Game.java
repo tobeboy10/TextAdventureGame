eaa001abb1462b363dda3191360659d37b7af949
Tobeboy10 <tobeboy10@gmail.com>
2020-01-20 21:38:54 -0800

    Added the first area of game progression.

--- Game.java
+++ Game.java
@@ -36,6 +36,7 @@ public class Game {
         System.out.println("You are well.");
         System.out.println("");
         saveGame();
+        playOuterGate();
     }
     
     private void saveGame() { //Overwrites save file "SaveGame.txt" using player data.
@@ -53,6 +54,8 @@ public class Game {
             writer.write("\r\n");
             writer.write(Integer.toString(player.getHealth()));
             writer.close();
+            System.out.println("Game saved.");
+            System.out.println("");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,8 +91,30 @@ public class Game {
         }
     }
 
-
     //TODO Game progression
 
-    
+    private void playOuterGate() { //Begins the actual game.
+        System.out.println("// Outer Gate to Ulibis \\\\");
+        System.out.println("Your journey begins at the Outer Gate to Ulibis. A gaurd stops you.");
+        System.out.println("\"Nobody enters the realm of Ulibis without a pass.\"");
+        System.out.println("Your Options:");
+        System.out.println("a) Show pass b) Draw weapon");
+        String input = reader.nextLine();
+        if (input.equals("a")) {
+            System.out.println("");
+            System.out.println("You show the gaurd your pass. He looks it over with a nod.");
+            System.out.println("\"Everything seems to be in order here. You may enter Ulibis, but don\'t cause any trouble.\"");
+        } else if (input.equals("b")) {
+            System.out.println("");
+            System.out.println("You draw your sword, the gaurd seems displeased.");
+            System.out.println("\"Not again. What you don\'t have a pass? Think you can take on the gaurd to get through? As if. Archers!\"");
+            System.out.println("Unknown to you, there were archers stationed in the upper levels of the Outer Gate to Ulibis.");
+            System.out.println("They begin to waves of fire arrows upon you.");
+            System.out.println("");
+            System.out.println("You have died. Reload to continue from where you left off.");
+        } else {
+            System.out.println("Invalid option. You have broke the game. Reload to continue from where you left off.");
+        }
+    }
+
 }
